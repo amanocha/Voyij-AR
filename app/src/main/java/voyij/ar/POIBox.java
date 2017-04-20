b18ab948336962999254205b0444e4fc2c646d4e
Sam Toffler <srt15@duke.edu>
2017-04-20 15:10:37 -0400

    ordered points and added height

---
+++ app/src/main/java/voyij/ar/POIBox.java
@@ -0,0 +1,44 @@
+package voyij.ar;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by samtoffler on 4/19/17.
+ */
+
+public class POIBox{
+    private ImageView imageView;
+    private TextView textViewTop;
+    private TextView textViewBottom;
+
+
+    public POIBox(String imageSrc, String textTop, String textBottom){
+        imageView = (ImageView) findViewById(R.drawable.building);
+        textViewTop = new TextView(
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public void setImageView(ImageView imageView) {
+        this.imageView = imageView;
+    }
+
+    public TextView getTextViewTop() {
+        return textViewTop;
+    }
+
+    public void setTextViewTop(TextView textViewTop) {
+        this.textViewTop = textViewTop;
+    }
+
+    public TextView getTextViewBottom() {
+        return textViewBottom;
+    }
+
+    public void setTextViewBottom(TextView textViewBottom) {
+        this.textViewBottom = textViewBottom;
+    }
+}
